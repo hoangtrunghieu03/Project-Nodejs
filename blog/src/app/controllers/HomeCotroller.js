d559b7cdeca59d91f8c7f3d17f2f91d793891787
Your Name <you@example.com>
2023-03-18 22:15:18 +0700

    uploadcode

--- blog/src/app/controllers/HomeCotroller.js
+++ blog/src/app/controllers/HomeCotroller.js
@@ -3,7 +3,6 @@ const { mutipleMongooseToObject } = require('../../util/mongoose');
 
 class HomeController {
     home (req, res, next) {
-        // res.render('home', { isLoggedIn: req.session.isLoggedIn})
         Course.find({})
             .then((courses) => {
                 res.render('home', {
--- blog/src/routes/index.js
+++ blog/src/routes/index.js
@@ -1,12 +1,14 @@
 const loginRouter = require('./login')
 const registerRouter = require('./register')
 const logoutRouter = require('./logout')
+const coursesRouter = require('./courses')
 const homeRouter= require('./home')
 
 function route(app) {
     app.use('/register', registerRouter)
     app.use('/login', loginRouter)
     app.use('/logout', logoutRouter)
+    app.use('/courses', coursesRouter)
     
     app.use('/', homeRouter)
 
